e8e22a07263c6b8f6ad90a020e9b105a1626ddda
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-26 12:29:34 -0700

    add 3 components into App create Search functional component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +7,30 @@ function App() {
     <div className="App">
       <header className="App-header">
        <h1> <img src={logo} className="App-logo" alt="logo" />PERN Contacts</h1>
-       
+       <Search />
+       <AddContact />
+       <ViewAll />
       </header>
     </div>
   );
 }
+
+const Search = (props) => {
+  return(
+    <form>
+      <label>Search
+        <input 
+          type="text"
+        />
+      </label>
+      <button 
+        onClick = {something}
+      />
+    </form>
+  );
+}
+
+
+
 
 export default App;
